4ceb4917819ef6fbfaae7dbb328606e3b6e260fe
ARTSIOM KUKHAREV <xeontem@gmail.com>
2017-01-28 02:08:25 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.result = 0;
+        this.min = 0;
+        this.max = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.result = Math.ceil((this.min + this.max)/2);//
+		return this.result;
     }
 
     lower() {
-
+        this.max = this.result;
     }
 
     greater() {
-
+        this.min = this.result;
     }
 }
 
