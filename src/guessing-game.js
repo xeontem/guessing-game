2ac14a92bc25e538204765bc0567f41701e32a46
UNBREAKING <apaseishvili@gmail.com>
2017-01-28 01:46:02 +0300

    wtf

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min=0;
+        this.max=0;
+        this.num=0;
+    }
 
     setRange(min, max) {
-
+        this.min=min;
+        this.max=max-1;
     }
 
     guess() {
-
+         this.num=Math.floor((this.min+this.max)/2)+1;
+        return this.num;
     }
 
     lower() {
-
+        this.max=this.num-1;
     }
 
     greater() {
-
+        this.min=this.num;
     }
 }
 
